22aa1be6b738ece24b006ef0e47de6344756e42c
Troy Shu <tmshu1@gmail.com>
2012-08-14 15:27:23 -0400

    add main timer, button, basic styling

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -13,13 +13,35 @@ describe "Static pages" do
     end
 
     describe "nav links" do
-		it "should go to home/root" do
+		describe "should go to home/root when Home clicked" do
 			before { click_link "Home" }
 			it { should have_selector('title', text: full_title('')) }
+
+		end
+		
+		describe "should go to home/root when logo clicked" do
+			before { click_link "logo" }
+			it { should have_selector('title', text: full_title('')) }
+
 		end
 
+		describe "should go to blog" do
+			before { click_link "Blog" }
+			it { should have_content('blog') }
+		end
+
+		describe "should go to help page" do
+			before { click_link "Help" }
+			it { should have_selector('title', text: full_title('Help')) }
+		end
 
     end
+
+	describe "footer links" do
+		before { click_link "Contact" }
+		it { should have_selector('title', text: full_title('Contact')) }
+	end
+
   end
 
 end
