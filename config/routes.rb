cb4bd1089db1b3812e6b1113ee4e56c2b6c161ae
Troy Shu <tmshu1@gmail.com>
2012-08-19 19:32:37 -0400

    fix contact route (still need to make contact page)

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ PomodoroApp::Application.routes.draw do
 
   match '/signup',  to: 'users#new'
   match '/help', to: 'static_pages#help'
-  match '/contact' to: 'static_pages#contact'
+  match '/contact', to: 'static_pages#contact'
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
