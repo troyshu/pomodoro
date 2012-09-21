78905c2a843a863ffcf097d9d0e6ef80a5f92400
Troy Shu <tmshu1@gmail.com>
2012-09-20 22:10:14 -0400

    disable premium upgrade for now, want to gauge interest while doing final testing and waiting for dns redirect

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,7 @@ PomodoroApp::Application.routes.draw do
   match '/pricing', to: 'static_pages#pricing'
   match '/timer', to: 'static_pages#timer'
   match '/getpremium', to: 'static_pages#getpremium'
+
   
   # The priority is based upon order of creation:
   # first created -> highest priority.
